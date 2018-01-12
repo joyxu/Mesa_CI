49a96002b8381094dafddbea2813e707bd0f6d4e
Dylan Baker <dylan@pnwbakers.com>
2018-01-12 10:11:14 -0800

    drm: Add support for using meson to build libdrm

--- drm/build.py
+++ drm/build.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "..", "repos", "mesa_ci"))
 import build_support as bs
@@ -20,5 +19,28 @@ class DrmBuilder(bs.AutoBuilder):
         # wait for.
         pass
 
-bs.build(DrmBuilder())
 
+def meson_build():
+    global_opts = bs.Options()
+
+    options = [
+        '-Detnaviv=true',
+        '-Dfreedreno=true',
+    ]
+    if global_opts.config != 'debug':
+        options.extend(['-Dbuildtype=release', '-Db_ndebug=true'])
+    b = bs.builders.MesonBuilder(extra_definitions=options, install=True)
+    bs.build(b)
+
+
+def main():
+    pm = bs.ProjectMap()
+    sd = pm.project_source_dir(pm.current_project())
+    if os.path.exists(os.path.join(sd, 'meson.build')):
+        meson_build()
+    else:
+        bs.build(MesaBuilder())
+
+
+if __name__ == '__main__':
+    main()
