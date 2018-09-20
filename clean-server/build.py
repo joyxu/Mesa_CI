7fde015fc5dda65abb27ed26e276a4fb7b195d4a
Mark Janes <mark.a.janes@intel.com>
2018-09-20 13:48:03 -0700

    clean-server: increase the number of builds kept on the server
    20 days uses 15% of available space.

--- clean-server/build.py
+++ clean-server/build.py
@@ -54,5 +54,5 @@ for a_dir in os.listdir(result_path):
         build_dir = sub_dir + "/" + a_build_dir
         if os.path.islink(build_dir):
             continue
-        if file_age_in_days(build_dir) > 20:
+        if file_age_in_days(build_dir) > 50:
             bs.rmtree(build_dir)
