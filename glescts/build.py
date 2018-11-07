a0a988d32ce118608428536d4df82e16813507ff
Mark Janes <mark.a.janes@intel.com>
2018-11-06 16:51:59 -0800

    glescts: use a local git checkout instead of a symlink
    This allows each project to configure different external revisions.

--- glescts/build.py
+++ glescts/build.py
@@ -4,4 +4,9 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "..", "repos", "mesa_ci"))
 import build_support as bs
 
-bs.build(bs.CtsBuilder(suite="es"))
+def get_external_revisions(revisions_dict=None):
+    return bs.deqp_external_revisions(project="glescts",
+                                      revisions_dict=revisions_dict)
+
+if __name__ == "__main__":
+    bs.build(bs.CtsBuilder(suite="es"))
