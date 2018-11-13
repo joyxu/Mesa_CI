b63593496395c7119514e2619fe401c3406586bd
Mark Janes <mark.a.janes@intel.com>
2018-11-13 11:41:14 -0800

    piglit: support vkrunner tests

---
+++ vkrunner/build.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys, os, importlib, git
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "..", "repos", "mesa_ci"))
+import build_support as bs
+
+if __name__ == '__main__':
+    bs.build(bs.CMakeBuilder())
+
