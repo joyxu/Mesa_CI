6cde5e98abb75f9644483eeb11c70cbc8e2bbdc4
Mark Janes <mark.a.janes@intel.com>
2019-01-16 11:34:08 -0800

    deqp: move lister to build_support
    The DeqpLister class needs to be sharable with iris variants of the
    deqp test class.

--- deqp-test/build.py
+++ deqp-test/build.py
@@ -15,72 +15,6 @@ class SlowTimeout:
     def GetDuration(self):
         return 500
 
-class DeqpLister(object):
-    def __init__(self, binary):
-        self.binary = binary
-        self.o = bs.Options()
-        self.pm = bs.ProjectMap()
-        self.version = None
-
-    def tests(self, env):
-        # don't execute tests that are part of the other suite
-        whitelist_txt = None
-        cases_xml = None
-        if "gles2" in self.binary:
-            whitelist_txt = self.pm.project_source_dir("deqp") + "/android/cts/master/gles2-master.txt"
-            cases_xml = "dEQP-GLES2-cases.xml"
-        if "gles3" in self.binary:
-            whitelist_txt = self.pm.project_source_dir("deqp") + "/android/cts/master/gles3-master.txt"
-            cases_xml = "dEQP-GLES3-cases.xml"
-        if "gles31" in self.binary:
-            whitelist_txt = self.pm.project_source_dir("deqp") + "/android/cts/master/gles31-master.txt"
-            cases_xml = "dEQP-GLES31-cases.xml"
-        if "egl" in self.binary:
-            whitelist_txt = self.pm.project_source_dir("deqp") + "/android/cts/master/egl-master.txt"
-            cases_xml = "dEQP-EGL-cases.xml"
-        deqp_dir = os.path.dirname(self.binary)
-        os.chdir(deqp_dir)
-        cmd = [self.binary,
-               "--deqp-runmode=xml-caselist"]
-        bs.run_batch_command(cmd, env=env)
-        all_tests = bs.DeqpTrie()
-        all_tests.add_xml(cases_xml)
-        whitelist = bs.DeqpTrie()
-        whitelist.add_txt(whitelist_txt)
-        all_tests.filter_whitelist(whitelist)
-        os.chdir(self.pm.project_build_dir())
-        return all_tests
-
-    def blacklist(self, all_tests):
-        bd = self.pm.project_build_dir()
-        # to match "skl" instead of "sklgt2"
-        hw_prefix = self.o.hardware[:3]
-        for a_blacklist in [self.o.hardware + "_blacklist.txt",
-                            hw_prefix + "_blacklist.txt",
-                            "blacklist.txt"]:
-            blacklist_txt = bd + a_blacklist
-            if blacklist_txt:
-                blacklist_trie = bs.DeqpTrie()
-                blacklist_trie.add_txt(blacklist_txt)
-                all_tests.filter(blacklist_trie)
-            
-        if not self.version:
-            self.version = bs.mesa_version()
-
-        # filter immediately, since any unsupported tests under these
-        # top-level categories will prevent them from being filtered.
-        if "gles2" not in self.binary:
-            all_tests.filter(["dEQP-GLES2"])
-        if "gles31" not in self.binary:
-            all_tests.filter(["dEQP-GLES31"])
-        if "gles3" not in self.binary:
-            all_tests.filter(["dEQP-GLES3"])
-        if "egl" not in self.binary:
-            all_tests.filter(["dEQP-EGL"])
-
-        if "daily" != self.o.type and not self.o.retest_path:
-            # these tests triple the run-time
-            all_tests.filter(["dEQP-GLES31.functional.copy_image"])
         
 class DeqpBuilder(object):
     def __init__(self):
@@ -123,7 +57,7 @@ class DeqpBuilder(object):
         for module in modules:
             binary = self.pm.build_root() + "/opt/deqp/modules/" + module + "/deqp-" + module
             results = t.test(binary,
-                             DeqpLister(binary),
+                             bs.DeqpSuiteLister(binary),
                              [],
                              self.env)
             all_results.merge(results)
