4b166703796c97c415988d1ca23010944e5761ea
Clayton Craft <clayton.a.craft@intel.com>
2019-02-06 15:29:02 -0800

    skqp: rely on fetch_sources for handling external dependencies

--- skqp/build.py
+++ skqp/build.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
-import git
 import importlib
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "..", "repos", "mesa_ci"))
 import build_support as bs
+
+
+def get_external_revisions(revisions_dict=None):
+    return bs.skqp_external_revisions(project="skqp",
+                                      revisions_dict=revisions_dict)
+
 
 if __name__ == "__main__":
     bs.build(bs.SkqpBuilder())
