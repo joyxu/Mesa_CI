827ba9a805cdb1613c7368f8fec77dfd4abddc5e
Clayton Craft <clayton.a.craft@intel.com>
2019-03-15 13:18:10 -0700

    android-buildtest: Do not run on Mesa versions older than 19.1
    The recent update to a newer android source tree means that the android
    build test is unable to build Mesa older than 19.1 since these stable
    releases do not include the initial Iris driver patch series that
    required the newer Android source tree.

--- android-buildtest/build.py
+++ android-buildtest/build.py
@@ -15,6 +15,12 @@ def main():
     if not os.path.exists(os.path.join(sd,
                                        'src/mesa/drivers/osmesa/meson.build')):
         return 0
+    # The android source tree used does not support Mesa <19.1
+    if not os.path.exists(os.path.join(sd,
+                                       'docs/relnotes/19.1.0.html')):
+        print("Android build test does not support this version of Mesa. No "
+              "build test will be performed.")
+        return 0
 
     bs.build(bs.AndroidBuilder(src_location="~/android-ia",
                                module="libGLES_mesa"))
