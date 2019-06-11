2a7b3c0278421840780b07d3fa50d63451391936
Clayton Craft <clayton.a.craft@intel.com>
2019-06-11 14:04:42 -0700

    skqp: enable gen9 and gen9atom iris testing

--- skqp-test/build.py
+++ skqp-test/build.py
@@ -6,5 +6,8 @@ sys.path.append(path.join(path.dirname(path.abspath(sys.argv[0])), "..",
 import build_support as bs
 if __name__ == "__main__":
     env = {}
+    if 'iris' in bs.Options().hardware:
+        env['MESA_LOADER_DRIVER_OVERRIDE'] = 'iris'
+
     bs.Options().update_env(env)
     bs.build(bs.SkqpTester(env=env))
