e8dc16243d8275108d574b23f1fb231af9e5d464
Clayton Craft <clayton.a.craft@intel.com>
2019-09-20 12:17:24 -0700

    android-buildtest: update vulkan target
    This changed in android 10

--- android-buildtest/build.py
+++ android-buildtest/build.py
@@ -24,7 +24,7 @@ def main():
 
     bs.build(bs.AndroidBuilder(src_location="~/android-ia",
                                modules=["libGLES_mesa",
-                                        "vulkan.project-celadon"]))
+                                        "vulkan.broxton"]))
 
 
 if __name__ == '__main__':
