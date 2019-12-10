dcbaker/mesa_jenkins

scripts: Fix RevisionSpecification to make from_cmd_line a classmethod

scripts/bisect_project.py
@@ -38,7 +38,7 @@ spec_xml = pm.build_spec()
 results_dir = spec_xml.find("build_master").attrib["results_dir"]
 
 repos = bs.RepoSet()
-_revspec = bs.RevisionSpecification(from_cmd_line=revs)
+_revspec = bs.RevisionSpecification.from_cmd_line(revs)
 _revspec.checkout()
 
 proj_rev = args.good_rev.split("=")

scripts/build_jenkins.py
@@ -216,7 +216,7 @@ def main():
     bspec.checkout(branch)
     revspec = None
     if (revision):
-        revspec = bs.RevisionSpecification(from_cmd_line=revision.split())
+        revspec = bs.RevisionSpecification.from_cmd_line(revision.split())
         revspec.checkout()
 
     revspec = bs.RevisionSpecification()

scripts/build_perf.py
@@ -75,7 +75,7 @@ def main():
             commits.append(commit.hexsha)
         revision = "mesa=" + str(commits[int(random.random() * len(commits))])
         
-    revspec = bs.RevisionSpecification(from_cmd_line=revision.split())
+    revspec = bs.RevisionSpecification.from_cmd_line(revision.split())
     revspec.checkout()
 
     revspec = bs.RevisionSpecification()

scripts/jenkins_single_test.py
@@ -72,7 +72,7 @@ def main():
     bspec = bs.BuildSpecification()
     bspec.checkout(branch)
     if (revision):
-        revspec = bs.RevisionSpecification(from_cmd_line=revision.split())
+        revspec = bs.RevisionSpecification.from_cmd_line(revision.split())
         revspec.checkout()
 
     revspec = bs.RevisionSpecification()

joyxu/Mesa_CI

scripts/bisect_project.py
@@ -38,7 +38,7 @@ spec_xml = pm.build_spec()
 results_dir = spec_xml.find("build_master").attrib["results_dir"]
 
 repos = bs.RepoSet()
-_revspec = bs.RevisionSpecification(from_cmd_line=revs)
+_revspec = bs.RevisionSpecification.from_cmd_line_param(revs)
 _revspec.checkout()
 
 proj_rev = args.good_rev.split("=")

scripts/build_jenkins.py
@@ -216,7 +216,7 @@ def main():
     bspec.checkout(branch)
     revspec = None
     if (revision):
-        revspec = bs.RevisionSpecification(from_cmd_line=revision.split())
+        revspec = bs.RevisionSpecification.from_cmd_line_param(revision.split())
         revspec.checkout()
 
     revspec = bs.RevisionSpecification()

scripts/build_perf.py
@@ -75,7 +75,7 @@ def main():
             commits.append(commit.hexsha)
         revision = "mesa=" + str(commits[int(random.random() * len(commits))])
         
-    revspec = bs.RevisionSpecification(from_cmd_line=revision.split())
+    revspec = bs.RevisionSpecification.from_cmd_line_param(revision.split())
     revspec.checkout()
 
     revspec = bs.RevisionSpecification()

scripts/jenkins_single_test.py
@@ -72,7 +72,7 @@ def main():
     bspec = bs.BuildSpecification()
     bspec.checkout(branch)
     if (revision):
-        revspec = bs.RevisionSpecification(from_cmd_line=revision.split())
+        revspec = bs.RevisionSpecification.from_cmd_line_param(revision.split())
         revspec.checkout()
 
     revspec = bs.RevisionSpecification()