151b21e2df886d9fed30e6c460adcba585039a4d
Mark Janes <markjanes@swizzler.org>
2020-01-15 11:08:47 -0800

    deqp-runtime: add bsw to test platforms
    Subsequent changes to dEQP have eliminated the tests that took too
    long.

--- deqp-runtime/build.py
+++ deqp-runtime/build.py
@@ -29,9 +29,7 @@ class DeqpRuntimeLister():
         self.version = None
         bd = self.pm.project_build_dir()
         hw_prefix = self.o.hardware
-        # Note: bsw has known failures that need to be resolved
-        # first: https://bugs.freedesktop.org/show_bug.cgi?id=104981
-        if hw_prefix in ['g33', 'g45', 'ilk', 'g965', 'hsw', 'byt', 'bsw']:
+        if hw_prefix in ['g33', 'g45', 'ilk', 'g965', 'hsw', 'byt']:
             raise Exception(("ERROR: This platform is not supported by "
                              "this test"))
         if "gles2" in self.binary:
