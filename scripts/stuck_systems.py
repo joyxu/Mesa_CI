a67dff61fcbf265c123aac411e37fd541e3ec75e
Clayton Craft <clayton.a.craft@intel.com>
2020-03-31 12:54:20 -0700

    scripts/stuck_systems: look in entire system list
    This changes the script to query the entire list of systems for status
    instead of looking in the queue for systems with stuck jobs.

--- scripts/stuck_systems.py
+++ scripts/stuck_systems.py
@@ -10,44 +10,29 @@ offline_causes = ["hudson.node_monitors.ResponseTimeMonitor$Data",
                   "hudson.slaves.OfflineCause$LaunchFailed"]
 
 
-def get_offline_reason(jenkins_server, computer):
-    try:
-        resp = urlopen('http://' + jenkins_server + '/computer/' + computer
-                       + '/api/json').read()
-    except HTTPError as e:
-        print('ERR: Unable to reach url: http://' + jenkins_server +
-              '/computer/' + computer + 'api/json')
-        print(e)
-        return None
-    cause = json.loads(resp).get('offlineCause')
-    if cause:
-        return cause.get('_class')
-    return None
-
-
 def list_stuck_systems():
     spec = ProjectMap().build_spec()
     jenkins_server = spec.find("build_master").attrib["host"]
-    resp = urlopen('http://' + jenkins_server + '/queue/api/json').read()
-    queued_builds = json.loads(resp)['items']
+    resp = urlopen('http://' + jenkins_server + '/computer/api/json').read()
+    nodes = json.loads(resp)['computer']
 
     stuck_systems = {}
-    for build in queued_builds:
-        if build['task']['name'] == 'reboot_single':
+    for node in nodes:
+        if node['_class'] != 'hudson.slaves.SlaveComputer':
+            continue
+        if node['offline'] and node['offlineCause']['_class'] in offline_causes:
             try:
-                computer = build['actions'][0]['parameters'][0]['value']
+                node_name = node['displayName']
             except (IndexError, KeyError) as e:
                 print('ERR: Unable to parse computer name: ')
                 print(e)
                 continue
-            cause = get_offline_reason(jenkins_server, computer)
-            if not cause:
-                continue
-            if cause in offline_causes and computer not in stuck_systems:
-                stuck_systems[computer] = ('http://' + jenkins_server +
-                                           '/computer/' + computer + '/')
-    for c, u in stuck_systems.items():
-        print('{:<15}{}'.format(c, u))
+            stuck_systems[node_name] = ('http://' + jenkins_server +
+                                        '/computer/' + node_name + '/')
+    sorted_systems = sorted(stuck_systems)
+    for n in sorted_systems:
+        print('{:<15}{}'.format(n, stuck_systems[n]))
+    print('total: ' + str(len(sorted_systems)))
 
 
 if __name__ == "__main__":
