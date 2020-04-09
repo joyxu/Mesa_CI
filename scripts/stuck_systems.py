ae35d6b1a5e158c1b211c8bf5970cf5bdcb78bc6
Clayton Craft <clayton.a.craft@intel.com>
2020-04-09 10:14:28 -0700

    scripts/stuck_systems: add option for opening jenkins node urls
    The '-b' option added here will open every jenkins node url returned by
    the script in the web browser.
    
    Note: this works best when the browser is already running.

--- scripts/stuck_systems.py
+++ scripts/stuck_systems.py
@@ -1,6 +1,8 @@
+import argparse
 import json
 import os
 import sys
+import webbrowser
 from urllib.request import urlopen, URLError, HTTPError
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])),
                              "..", "repos", "mesa_ci", "build_support"))
@@ -31,12 +33,29 @@ def list_stuck_systems():
                 continue
             stuck_systems[node_name] = ('http://' + jenkins_server +
                                         '/computer/' + node_name + '/')
+
+    return stuck_systems
+
+
+def main():
+    parser = argparse.ArgumentParser(description=("Queries jenkins for systems"
+                                                  " that are not responding."))
+    parser.add_argument('-b', '--browser', action="store_true",
+                        help="Open all links to nodes in the default browser. "
+                        "This works best if the browser is already running.")
+    opts = parser.parse_args()
+
+    stuck_systems = list_stuck_systems()
     sorted_systems = sorted(stuck_systems)
     for n in sorted_systems:
         print('{:<15}{}'.format(n, stuck_systems[n]))
     print('total: ' + str(len(sorted_systems)))
+    
+    if opts.browser:
+        print("Openning urls in browser")
+        for url in stuck_systems.values():
+            webbrowser.open_new_tab(url)
 
 
 if __name__ == "__main__":
-    list_stuck_systems()
-
+    main()
