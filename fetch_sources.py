3e08b2724c3e6a56681d3375fcc3a09588d659ff
Clayton Craft <clayton.a.craft@intel.com>
2020-05-15 11:15:52 -0700

    fetch_sources: prune repos during fetch
    This eliminates fetch failures due to local refs becoming invalid refs
    on the remote

--- fetch_sources.py
+++ fetch_sources.py
@@ -206,7 +206,7 @@ def main():
 
     fail = True
     for i in range(15):
-        repos.fetch(limit_to_repos)
+        repos.fetch(limit_to_repos, prune=True)
         try:
             print("Checking out specified commit (try {}/15)".format(i+1))
             repos.checkout(limit_to_repos)
