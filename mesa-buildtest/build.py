006b92f172d39ae9ee3701a04c1e82da85f9651d
Nico <nicolas.g.cortes@intel.com>
2021-10-22 17:13:11 -0700

    mesa-buildtest: remove extraneous test lists after mesa=29177c7ceee351ea9cfa4a0c3f81ae2c70c83d9c

--- mesa-buildtest/build.py
+++ mesa-buildtest/build.py
@@ -68,59 +68,6 @@ def main():
         options.extend(['-Dbuildtype=release', '-Db_ndebug=true'])
     b = MesonBuilder(extra_definitions=options, install=False)
 
-    b.tests += [
-        # TODO: These need runtime discovery, probably using `find` or to point
-        # at the DSOs in the install directory
-        #
-        #'es1-ABI-check',
-        #'es2-ABI-check',
-        #'gbm-symbols-check',
-        #'wayland-egl-symbols-check',
-        #'wayland-egl-abi-check',
-        #'egl-symbols-check',
-        #'egl-entrypoint-check',
-
-        'anv_block_pool_no_free',
-        'anv_state_pool',
-        'anv_state_pool_free_list_only',
-        'anv_state_pool_no_free',
-        'blob_test',
-        'cache_test',
-        'clear',
-        'collision',
-        'delete_and_lookup',
-        'delete_management',
-        'destroy_callback',
-        'glx-dispatch-index-check',
-        'insert_and_lookup',
-        'insert_many',
-        'isl_surf_get_image_offset',
-        'lp_test_arit',
-        'lp_test_blend',
-        'lp_test_conv',
-        'lp_test_format',
-        'lp_test_printf',
-        'mesa-sha1',
-        'null_destroy',
-        'random_entry',
-        'remove_null',
-        'replacement',
-        'roundeven',
-        'u_atomic',
-    ]
-
-    b.gtests += [
-        'general_ir_test',
-        'glx-test',
-        'intel_compiler_tests'
-        'main-test',
-        'nir_control_flow',
-        'sampler_types_test',
-        'shared-glapi-test',
-        'string_buffer',
-        'uniform_initializer_test',
-    ]
-
     try:
         build(b)
     except subprocess.CalledProcessError as e:
